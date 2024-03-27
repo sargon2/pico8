8251d1f28be74e1f256eef3fa11adadd7bd8ecb9
David Besen <dbesen@gmail.com>
2024-03-27 15:10:36 -0600

    Make push to windows retry

--- push-to-windows.sh
+++ push-to-windows.sh
@@ -1,4 +1,15 @@
-#!/bin/bash -ex
+#!/bin/bash -x
 
-rm -rf /mnt/c/Users/dbese/Dropbox/pico8/root/mine
+# For some reason removing stuff from Dropbox on the windows mount causes intermittent failures.
+# So just retry a few times.
+max_retry=5
+counter=0
+until rm -rf /mnt/c/Users/dbese/Dropbox/pico8/root/mine
+do
+    sleep 0.1
+    [[ counter -eq $max_retry ]] && echo "Failed!" && exit 1
+    echo "Trying again. Try #$counter"
+    ((counter++))
+done
+
 cp -r mine /mnt/c/Users/dbese/Dropbox/pico8/root/
