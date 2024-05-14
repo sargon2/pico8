a1960a8259659fe6a523f5b46855580f6a0c55c2
David Besen <dbesen@gmail.com>
2024-05-14 13:24:19 -0600

    Simplify sfx control code

--- playground/solar_sally/components/SFX.lua
+++ playground/solar_sally/components/SFX.lua
@@ -1,8 +1,5 @@
 --[[const]] SFX_id_footsteps = 0
 
-local sfx_loops = {} -- sfx_loops[loop_id] = coroutine
-local next_loop_id = 1
-
 function start_sfx_loop(sfx_id, frame_count)
     if(not Settings_60fps) frame_count /= 2
     local coroutine = CoroutineRunner_StartScript(function ()
@@ -13,14 +10,10 @@ function start_sfx_loop(sfx_id, frame_count)
             end
         end
     end)
-    local loop_id = next_loop_id
-    next_loop_id += 1
-    sfx_loops[loop_id] = coroutine
-    return loop_id
+    return coroutine
 end
 
-function stop_sfx_loop(loop_id)
-    if(loop_id == nil) return
-    CoroutineRunner_Cancel(sfx_loops[loop_id])
-    sfx_loops[loop_id] = nil
+function stop_sfx_loop(coroutine)
+    if(coroutine == nil) return
+    CoroutineRunner_Cancel(coroutine)
 end
