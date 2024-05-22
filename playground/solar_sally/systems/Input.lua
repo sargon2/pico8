a04658c8e7fbb21f78bd19ab812b7cec17250e9d
David Besen <dbesen@gmail.com>
2024-05-22 14:27:26 -0600

    Fix shrinko8 button constants

--- playground/solar_sally/systems/Input.lua
+++ playground/solar_sally/systems/Input.lua
@@ -1,8 +1,10 @@
 -- On mobile, x is on the left and o is on the right.
 -- On a pc, x is x and o is z, so x is on the right and o is on the left.
 
---[[const]] Button_select_action = ❎
---[[const]] Button_take_action = 🅾️
+-- We can't use the predefined button constants (❎, 🅾️) because shrinko8 doesn't recognize them for constants.
+-- o is 4 and x is 5.
+--[[const]] Button_select_action = 5
+--[[const]] Button_take_action = 4
 
 local Input_AllInputDisabled = false
 
