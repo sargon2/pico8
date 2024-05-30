2f10ddd146d52f5ded4b1cd7953d096b22f09133
David Besen <dbesen@gmail.com>
2024-05-30 15:12:51 -0600

    Update title screen (clobbering map data!)

--- playground/solar_sally/systems/TitleScreen.lua
+++ playground/solar_sally/systems/TitleScreen.lua
@@ -10,6 +10,21 @@ end
 function TitleScreen.draw()
     -- Copy working memory back to the screen to redraw it after the main loop's cls()
     memcpy(0x6000, 0x8000, 0x2000)
+
+    -- Superimpose text
+    --[[const]] local text_u = 120
+    --[[const]] local text_r = 31
+    print_outlined("press ", text_r, text_u, 7)
+    print_outlined(Button_display_take_action, text_r+23, text_u, 8)
+    print_outlined("to start", text_r+34, text_u, 7)
+end
+
+-- from https://www.lexaloffle.com/bbs/?tid=45020
+function print_outlined(s,...)
+    for i in all(split'\-f\f0,\-h\f0,\|f\f0,\|h\f0') do
+    ?i..s,...
+    end
+    ?s,...
 end
 
 function TitleScreen.update()
