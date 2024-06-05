f063c60c7bdc6e43bf158857abd2cf56eee85a0b
David Besen <dbesen@gmail.com>
2024-06-05 14:43:26 -0600

    Update title screen interlude to shel silverstein quote

--- playground/solar_sally/systems/TitleScreenInterlude.lua
+++ playground/solar_sally/systems/TitleScreenInterlude.lua
@@ -1,16 +1,17 @@
 TitleScreenInterlude = {}
 
 function TitleScreenInterlude.draw()
-    print("oNCE UPON A TIME,", 8, 16, 6)
-    print("THERE LIVED A YOUNG WOMAN", 16, 24, 6)
-    print("WITH A PROFOUND LOVE FOR", 16, 32, 6)
-    print("THE WORLD.", 16, 40, 6)
-
-    print("tHOUGH SHE WAS TOUCHED BY", 8, 56, 6)
-    print("GRAVE MISFORTUNE,", 16, 64, 6)
-
-    print("SHE COULD NOT FORGET", 8, 80, 6)
-    print("HER LOVE.", 16, 88, 6)
+    --[[const]] local top = 27
+    --[[const]] local left = 26
+    --[[const]] local left_indented = 36
+    --[[const]] local left_sig = 38
+    print("a PIECE OF SKY", left, top, 6)
+    print("bROKE OFF AND FELL", left, top+8, 6)
+    print("tHROUGH THE CRACK", left, top+16, 6)
+    print("IN THE CEILING", left_indented, top+24, 6)
+    print("rIGHT INTO MY SOUP,", left, top+32, 6)
+    print("kerplop!", left, top+40, 6)
+    print("- sHEL sILVERSTEIN", left_sig, top+54, 6)
 
     print("press "..Button_display_take_action.." to continue", 25, 118, 10)
 end
