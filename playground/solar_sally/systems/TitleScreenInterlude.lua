03a9d7e4689f074037384287bfdc0f937b574aa2
David Besen <dbesen@gmail.com>
2024-09-27 19:09:46 -0600

    Redact poem

--- playground/solar_sally/systems/TitleScreenInterlude.lua
+++ playground/solar_sally/systems/TitleScreenInterlude.lua
@@ -5,13 +5,14 @@ function TitleScreenInterlude.draw()
     --[[const]] local left = 26
     --[[const]] local left_indented = 36
     --[[const]] local left_sig = 38
-    print("a PIECE OF SKY", left, top, 6)
-    print("bROKE OFF AND FELL", left, top+8, 6)
-    print("tHROUGH THE CRACK", left, top+16, 6)
-    print("IN THE CEILING", left_indented, top+24, 6)
-    print("rIGHT INTO MY SOUP,", left, top+32, 6)
-    print("kerplop!", left, top+40, 6)
-    print("- sHEL sILVERSTEIN", left_sig, top+54, 6)
+    print("poem redacted", left, top, 6)
+    -- print("a PIECE OF SKY", left, top, 6)
+    -- print("bROKE OFF AND FELL", left, top+8, 6)
+    -- print("tHROUGH THE CRACK", left, top+16, 6)
+    -- print("IN THE CEILING", left_indented, top+24, 6)
+    -- print("rIGHT INTO MY SOUP,", left, top+32, 6)
+    -- print("kerplop!", left, top+40, 6)
+    -- print("- sHEL sILVERSTEIN", left_sig, top+54, 6)
 
     print("press "..Button_display_take_action.." to continue", 25, 118, 10)
 end
